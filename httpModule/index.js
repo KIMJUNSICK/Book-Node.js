e3a5c1a90e393dfa6134a558db31740b81b21dd2
Kim Jun Sik <jinaganda5@gmail.com>
2019-10-10 21:33:39 +0900

    Login with Cookie

--- httpModule/index.js
+++ httpModule/index.js
@@ -1,5 +1,9 @@
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const qs = require("querystring");
 
+// Ftn for parsing cookie
 const parseCookies = (cookie = "") =>
   cookie
     .split(";")
@@ -12,8 +16,29 @@ const parseCookies = (cookie = "") =>
 const server = http.createServer((req, res) => {
   const cookies = parseCookies(req.headers.cookie);
   console.log(req.url, cookies);
-  res.writeHead(200, { "Set-Cookie": "mycookie=test" });
-  res.end("Hello Cookie");
+  if (req.url.startsWith("/login")) {
+    const { query } = url.parse(req.url);
+    const { name } = qs.parse(query);
+    const expires = new Date();
+    expires.setMinutes(expires.getMinutes() + 5);
+    res.writeHead(302, {
+      Location: "/",
+      "Set-Cookie": `name=${encodeURIComponent(
+        name
+      )}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`
+    });
+    res.end();
+  } else if (cookies.name) {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(`${cookies.name}님 안녕하세요`);
+  } else {
+    fs.readFile("./index.html", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.end(data);
+    });
+  }
 });
 
 server.listen(8080);
