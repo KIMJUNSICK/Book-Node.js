d5d671eb2120f46e26b76503f42977090f5683a5
Kim Jun Sik <jinaganda5@gmail.com>
2019-10-16 09:31:59 +0900

    basic server

--- nodeBird/app.js
+++ nodeBird/app.js
@@ -1,5 +1,58 @@
+import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import logger from "morgan";
+import session from "express-session";
+import flash from "connect-flash";
 
+// .env
+dotenv.config();
+
+// express
 const app = express();
 
-app.listen(4000, () => console.log("junsik"));
+// set
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// middeware
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false
+    }
+  })
+);
+app.use(flash());
+
+// router
+
+// 404 middleware
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+// start server
+app.listen(process.env.PORT, () =>
+  console.log("Listening on: ✅  http://localhost:4000")
+);
