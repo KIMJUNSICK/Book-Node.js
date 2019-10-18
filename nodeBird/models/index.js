b43b7cee05d20fcaaf17c6504d9beb7326a7f575
Kim Jun Sik <jinaganda5@gmail.com>
2019-10-18 20:30:33 +0900

    Setting for Creating DB

--- nodeBird/models/index.js
+++ nodeBird/models/index.js
@@ -1,37 +1,44 @@
-'use strict';
+import Sequelize from "sequelize";
+import Config from "../config/config";
+import User from "./User";
+import Post from "./Post";
+import HashTag from "./HashTag";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = Config[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+db.User = User(sequelize, Sequelize);
+db.Post = Post(sequelize, Sequelize);
+db.HashTag = HashTag(sequelize, Sequelize);
 
-module.exports = db;
+// Relationship: 1:N
+db.User.hasMany(db.Post);
+db.Post.belongsTo(db.User);
+
+// M:N
+db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" });
+db.Hashtag.belongsToMany(db.Post, { through: "PostHashtag" });
+
+// M:M
+db.User.belongsToMany(db.User, {
+  foreignKey: "followingId",
+  as: "Followers",
+  through: "Follow"
+});
+db.User.belongsToMany(db.User, {
+  foreignKey: "followerId",
+  as: "Followings",
+  through: "Follow"
+});
+
+export default db;
