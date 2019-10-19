bbd2ece0c93233a1ed5ac800d01ee61399bbcb85
Kim Jun Sik <jinaganda5@gmail.com>
2019-10-19 10:11:32 +0900

    Custom Middleware for LogIn

--- nodeBird/routes/page.js
+++ nodeBird/routes/page.js
@@ -1,4 +1,5 @@
-const express = require("express");
+import express from "express";
+import { isLoggedIn, isNotLoggedIn } from "../middlewares";
 
 const router = express.Router();
 
@@ -6,21 +7,21 @@ router.get("/", (req, res) => {
   res.render("main", {
     title: "NodeBird",
     twits: [],
-    user: null,
+    user: req.user,
     loginError: req.flash("loginError")
   });
 });
 
-router.get("/join", (req, res) => {
+router.get("/join", isNotLoggedIn, (req, res) => {
   res.render("join", {
     title: "회원가입 - NodeBird",
-    user: null,
+    user: req.user,
     joinError: req.flash("joinError")
   });
 });
 
-router.get("/profile", (req, res) => {
-  res.render("profile", { title: "내 정보 - NodeBird", user: null });
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile", { title: "내 정보 - NodeBird", user: req.user });
 });
 
 module.exports = router;
