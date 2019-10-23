b6e02391edf1867426d32254ae19790efa6f7d9f
Kim Jun Sik <jinaganda5@gmail.com>
2019-10-23 09:22:58 +0900

    Add routes for servicing more data

--- nodebird-api/routers/v1.js
+++ nodebird-api/routers/v1.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import { verifyToken } from "../middlewares";
 import db from "../models";
 
-const { User, Domain } = db;
+const { User, Domain, Post, HashTag } = db;
 
 const router = express.Router();
 
@@ -50,6 +50,49 @@ router.post("/token", async (req, res) => {
 
 router.get("/test", verifyToken, (req, res) => {
   res.json(req.decoded);
+});
+
+router.get("/posts/my", verifyToken, (req, res) => {
+  Post.findAll({ where: { userId: req.decoded.id } })
+    .then(posts => {
+      console.log(posts);
+      res.json({
+        code: 200,
+        payload: posts
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({
+        code: 500,
+        message: "Server Error"
+      });
+    });
+});
+
+router.get("/posts/hashtag/:title", verifyToken, async (req, res) => {
+  try {
+    const hashtag = await HashTag.findOne({
+      where: { title: req.params.title }
+    });
+    if (!hashtag) {
+      return res.status(404).json({
+        code: 404,
+        message: "Not Found"
+      });
+    }
+    const posts = await hashtag.getPosts();
+    return res.json({
+      code: 200,
+      payload: posts
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      code: 500,
+      message: "Server Error"
+    });
+  }
 });
 
 export default router;
