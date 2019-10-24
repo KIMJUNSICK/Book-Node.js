73cc4e0e4c22240bf9873ebea31474f250da074b
Kim Jun Sik <jinaganda5@gmail.com>
2019-10-24 10:53:20 +0900

    Gif Chat: basic Server

---
+++ gif-chat/app.js
@@ -0,0 +1,52 @@
+import express from "express";
+import path from "path";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import flash from "connect-flash";
+import indexRouter from "./routes";
+
+require("dotenv").config();
+
+const { PORT, COOKIE_SECRET } = process.env;
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false
+    }
+  })
+);
+app.use(flash());
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(PORT, () =>
+  console.log(`Listening on:✅  http://localhost:${PORT}`)
+);
