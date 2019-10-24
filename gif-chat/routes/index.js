a99f63caa504851c915eb0e3cc9ec645ee0a8316
Kim Jun Sik <jinaganda5@gmail.com>
2019-10-24 14:13:00 +0900

    Gif Chat: Client with Web Socket

--- gif-chat/routes/index.js
+++ gif-chat/routes/index.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Home"));
+router.get("/", (req, res) => res.render("index"));
 
 export default router;
